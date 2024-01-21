1cdd724cf59622f5a0f8ec573dad80e757d2b3f0
Dhani Novan <dhani.novan@steam.deck>
2024-01-21 19:00:10 +0700

    add test openGL

---
+++ test_opengl.go
@@ -0,0 +1,77 @@
+// author: HardWareGuy
+
+package main
+
+import (
+	"fmt"
+	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func main() {
+	var winTitle string = "Go-SDL2 + OpenGL"
+	var winWidth, winHeight int32 = 640, 480
+	var window *sdl.Window
+	var context sdl.GLContext
+	var event sdl.Event
+	var running bool
+	var err error
+
+	if err = sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err = sdl.CreateWindow(winTitle, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		winWidth, winHeight, sdl.WINDOW_OPENGL)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+	context, err = window.GLCreateContext()
+	if err != nil {
+		panic(err)
+	}
+	defer sdl.GLDeleteContext(context)
+
+	if err = gl.Init(); err != nil {
+		panic(err)
+	}
+	fmt.Printf("Version: %v\nRenderer: %v\n", gl.GoStr(gl.GetString(gl.VERSION)), gl.GoStr((gl.GetString(gl.RENDERER))))
+	fmt.Println("Press ESC to quit")
+
+	gl.Enable(gl.DEPTH_TEST)
+	gl.ClearColor(0.2, 0.2, 0.3, 1.0)
+	gl.ClearDepth(1)
+	gl.DepthFunc(gl.LEQUAL)
+	gl.Viewport(0, 0, int32(winWidth), int32(winHeight))
+
+	running = true
+	for running {
+		for event = sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch t := event.(type) {
+			case *sdl.QuitEvent:
+				running = false
+			case *sdl.KeyboardEvent:
+				if t.Keysym.Sym == 27 {
+					running = false
+				}
+			}
+		}
+		drawgl()
+		window.GLSwap()
+	}
+}
+
+func drawgl() {
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+	gl.Begin(gl.TRIANGLES)
+	gl.Color3f(1.0, 0.0, 0.0)
+	gl.Vertex2f(0.5, 0.0)
+	gl.Color3f(0.0, 1.0, 0.0)
+	gl.Vertex2f(-0.5, -0.5)
+	gl.Color3f(0.0, 0.0, 1.0)
+	gl.Vertex2f(-0.5, 0.5)
+	gl.End()
+}
